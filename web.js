64682ac41647b539af9a69987ed3e32919a0a9de
Ravi Lekkala <raviprasad.reddy@gmail.com>
2013-07-17 16:58:10 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 var buffer = new Buffer(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
